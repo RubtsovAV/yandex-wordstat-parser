2b895b4f580ca49f37dd9ddea784bb9eaac8dd96
Ivan <support@frallion.ru>
2018-04-02 23:55:24 +0300

    Reformat Code

--- src/Browser/Guzzle6.php
+++ src/Browser/Guzzle6.php
@@ -15,237 +15,238 @@ use Kurbits\JavaScript\NodeRunner;
 
 class Guzzle6 extends AbstractBrowser
 {
-	/**
-	 * @var \GuzzleHttp\Client
-	 */
-	protected $client;
+    /**
+     * @var \GuzzleHttp\Client
+     */
+    protected $client;
 
-	public function __construct(bool $ignoreSslErrors = false)
-	{
-		// Ignoring an SSL error for the traffic sniffer
-		$this->client = new Client([
-			'verify' => !$ignoreSslErrors
-		]);
-	}
+    public function __construct(bool $ignoreSslErrors = false)
+    {
+        // Ignoring an SSL error for the traffic sniffer
+        $this->client = new Client([
+            'verify' => !$ignoreSslErrors
+        ]);
+    }
 
-	/**
-	 * Send query by the user yandex
-	 * 
-	 * @param  \RubtsovAV\YandexWordstatParser\Query 	  $query
-	 * @param  \RubtsovAV\YandexWordstatParser\YandexUser $yandexUser
-	 * 
-	 * @return \RubtsovAV\YandexWordstatParser\Result                      
-	 */
-	public function send(Query $query, YandexUser $yandexUser)
-	{
-		$requestOptions = $this->createRequestOptions($query, $yandexUser);
+    /**
+     * Send query by the user yandex
+     *
+     * @param  \RubtsovAV\YandexWordstatParser\Query $query
+     * @param  \RubtsovAV\YandexWordstatParser\YandexUser $yandexUser
+     *
+     * @return \RubtsovAV\YandexWordstatParser\Result
+     */
+    public function send(Query $query, YandexUser $yandexUser)
+    {
+        $requestOptions = $this->createRequestOptions($query, $yandexUser);
 
-		try {
-			while (true) {
-				$response = $this->client->request(
-					'POST', 
-					'https://wordstat.yandex.ru/stat/words', 
-					$requestOptions
-				);
+        try {
+            while (true) {
+                $response = $this->client->request(
+                    'POST',
+                    'https://wordstat.yandex.ru/stat/words',
+                    $requestOptions
+                );
 
-				$responseData = json_decode((string) $response->getBody(), true);
+                $responseData = json_decode((string)$response->getBody(), true);
 
-				if (isset($responseData['need_login'])) {
-					$this->login($yandexUser);
-					$requestOptions = $this->createRequestOptions($query, $yandexUser);
-					continue;
-				}
+                if (isset($responseData['need_login'])) {
+                    $this->login($yandexUser);
+                    $requestOptions = $this->createRequestOptions($query, $yandexUser);
+                    continue;
+                }
 
-				if (isset($responseData['captcha'])) {
-					$captchaUri = 'http:' . $responseData['captcha']['url'];
-					$captchaKey = $responseData['captcha']['key'];
+                if (isset($responseData['captcha'])) {
+                    $captchaUri = 'http:' . $responseData['captcha']['url'];
+                    $captchaKey = $responseData['captcha']['key'];
 
-					$captcha = new CaptchaImage($captchaUri);
-	        		if (!$this->solveCaptcha($captcha)) {
-	        			throw new BrowserException('solve captcha failed');
-	        		}
+                    $captcha = new CaptchaImage($captchaUri);
+                    if (!$this->solveCaptcha($captcha)) {
+                        throw new BrowserException('solve captcha failed');
+                    }
 
-	        		$requestOptions['form_params']['captcha_key'] = $captchaKey;
-	        		$requestOptions['form_params']['captcha_value'] = $captcha->getAnswer();
-	        		continue;
-				}
+                    $requestOptions['form_params']['captcha_key'] = $captchaKey;
+                    $requestOptions['form_params']['captcha_value'] = $captcha->getAnswer();
+                    continue;
+                }
 
-				if (isset($responseData['data'])) {
-					return $this->createResult($responseData, $yandexUser);
-				}
+                if (isset($responseData['data'])) {
+                    return $this->createResult($responseData, $yandexUser);
+                }
 
-				break;
-			}
-		} catch (GuzzleException $ex) {
-			throw new BrowserException(
-				$ex->getMessage(),
-				$ex->getCode(),
-				$ex
-			);
-		}
-		
-		throw new WrongResponseException(
-			(string) $response->getBody(),
-			'unknown response',
-			$responseData->getStatusCode()
-		);
-	}
+                break;
+            }
+        } catch (GuzzleException $ex) {
+            throw new BrowserException(
+                $ex->getMessage(),
+                $ex->getCode(),
+                $ex
+            );
+        }
 
-	protected function createRequestOptions(
-		Query $query, 
-		YandexUser $yandexUser
-	){
-		$requestOptions = $this->getBaseRequestOptions($yandexUser);
+        throw new WrongResponseException(
+            (string)$response->getBody(),
+            'unknown response',
+            $responseData->getStatusCode()
+        );
+    }
 
-		$requestOptions['form_params'] = [
-	        'db' => '',
-			'filter' => 'all',
-			'map' => 'world',
-			'page' => $query->getPageNumber(),
-			'page_type' => 'words',
-			'period' => 'monthly',
-			'regions' => '',
-			'sort' => 'cnt',
-			'type' => 'list',
-			'words' => $query->getWords(),
-	    ];
+    protected function createRequestOptions(
+        Query $query,
+        YandexUser $yandexUser
+    )
+    {
+        $requestOptions = $this->getBaseRequestOptions($yandexUser);
 
-		if ($query->getRegions()) {
-			$regions = implode(',', $query->getRegions());
-			$requestOptions['form_params']['regions'] = $regions;
-		}
+        $requestOptions['form_params'] = [
+            'db' => '',
+            'filter' => 'all',
+            'map' => 'world',
+            'page' => $query->getPageNumber(),
+            'page_type' => 'words',
+            'period' => 'monthly',
+            'regions' => '',
+            'sort' => 'cnt',
+            'type' => 'list',
+            'words' => $query->getWords(),
+        ];
 
-		$requestOptions['headers'] += [
-			'Accept-Language' => 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
-			'X-Requested-With' => 'XMLHttpRequest',
-			'Referer' => 'https://wordstat.yandex.ru/',
-		];
+        if ($query->getRegions()) {
+            $regions = implode(',', $query->getRegions());
+            $requestOptions['form_params']['regions'] = $regions;
+        }
 
-		return $requestOptions;
-	}
+        $requestOptions['headers'] += [
+            'Accept-Language' => 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
+            'X-Requested-With' => 'XMLHttpRequest',
+            'Referer' => 'https://wordstat.yandex.ru/',
+        ];
 
-	protected function getBaseRequestOptions(YandexUser $yandexUser)
-	{
-		$requestOptions = [];
+        return $requestOptions;
+    }
 
-		$requestOptions['headers'] = [
-			'User-Agent' => $this->getUserAgent(),
-		];
+    protected function getBaseRequestOptions(YandexUser $yandexUser)
+    {
+        $requestOptions = [];
 
-		if ($storagePath = $yandexUser->getStoragePath()) {
-			$storagePath .= '/guzzle6';
-			if (!is_dir($storagePath)) {
-				mkdir($storagePath, 0777, true);
-			}
-			$filename = $storagePath . '/cookies.json';
-			$requestOptions['cookies'] = new FileCookieJar($filename, true);
-		}
+        $requestOptions['headers'] = [
+            'User-Agent' => $this->getUserAgent(),
+        ];
 
-		if ($proxy = $this->getProxy()) {
-			$requestOptions['proxy'] = $proxy->toString();
-		}
+        if ($storagePath = $yandexUser->getStoragePath()) {
+            $storagePath .= '/guzzle6';
+            if (!is_dir($storagePath)) {
+                mkdir($storagePath, 0777, true);
+            }
+            $filename = $storagePath . '/cookies.json';
+            $requestOptions['cookies'] = new FileCookieJar($filename, true);
+        }
 
-		if ($requestTimeout = $this->getRequestTimeout()) {
-			$requestOptions['timeout'] = $requestTimeout;
-			$requestOptions['connect_timeout'] = $requestTimeout;
-		}
+        if ($proxy = $this->getProxy()) {
+            $requestOptions['proxy'] = $proxy->toString();
+        }
 
-		return $requestOptions;
-	}
+        if ($requestTimeout = $this->getRequestTimeout()) {
+            $requestOptions['timeout'] = $requestTimeout;
+            $requestOptions['connect_timeout'] = $requestTimeout;
+        }
 
-	protected function login(YandexUser $yandexUser)
-	{
-		$requestOptions = $this->getBaseRequestOptions($yandexUser);
+        return $requestOptions;
+    }
 
-		$requestOptions['form_params'] = [
-			'login' => $yandexUser->getLogin(),
-			'passwd' => $yandexUser->getPassword(),
-			'timestamp' => time(),
-		];
+    protected function login(YandexUser $yandexUser)
+    {
+        $requestOptions = $this->getBaseRequestOptions($yandexUser);
 
-		$response = $this->client->request(
-			'POST', 
-			'https://passport.yandex.ru/passport?mode=auth&from=&retpath=https%3A%2F%2Fwordstat.yandex.ru%2F&twoweeks=yes', 
-			$requestOptions
-		);
+        $requestOptions['form_params'] = [
+            'login' => $yandexUser->getLogin(),
+            'passwd' => $yandexUser->getPassword(),
+            'timestamp' => time(),
+        ];
 
-		$responseBody = $response->getBody() . '';
-		if (strpos($responseBody, 'control__input_name_history-answer')) {
-			throw new BrowserException("the yandex user is banned");
-		}
-	}
+        $response = $this->client->request(
+            'POST',
+            'https://passport.yandex.ru/passport?mode=auth&from=&retpath=https%3A%2F%2Fwordstat.yandex.ru%2F&twoweeks=yes',
+            $requestOptions
+        );
 
-	protected function createResult(array $data)
-	{
-		$data = $this->decodeData($data);
+        $responseBody = $response->getBody() . '';
+        if (strpos($responseBody, 'control__input_name_history-answer')) {
+            throw new BrowserException("the yandex user is banned");
+        }
+    }
 
-		$impressions = 0;
-		$includingPhrases = [];
-		$phrasesAssociations = [];
-		$lastUpdate = 0;
-		$nextPageExists = (bool) $data['hasNextPage'];
+    protected function createResult(array $data)
+    {
+        $data = $this->decodeData($data);
 
-		foreach ($data['includingPhrases']['items'] as $phrase) {
-			$includingPhrases[] = [
-				'words' => $phrase['phrase'], 
-	 			'impressions' => (int) preg_replace('#[^0-9]#', '', $phrase['number']),
-			];
-		}
+        $impressions = 0;
+        $includingPhrases = [];
+        $phrasesAssociations = [];
+        $lastUpdate = 0;
+        $nextPageExists = (bool)$data['hasNextPage'];
 
-		if (count($includingPhrases) > 0) {
-			$impressions = $includingPhrases[0]['impressions'];
-		}
+        foreach ($data['includingPhrases']['items'] as $phrase) {
+            $includingPhrases[] = [
+                'words' => $phrase['phrase'],
+                'impressions' => (int)preg_replace('#[^0-9]#', '', $phrase['number']),
+            ];
+        }
 
-		foreach ($data['phrasesAssociations']['items'] as $phrase) {
-			$phrasesAssociations[] = [
-				'words' => $phrase['phrase'], 
-	 			'impressions' => (int) preg_replace('#[^0-9]#', '', $phrase['number']),
-			];
-		}
+        if (count($includingPhrases) > 0) {
+            $impressions = $includingPhrases[0]['impressions'];
+        }
 
-		preg_match(
-			'#(?<day>\d+)\.(?<month>\d+)\.(?<year>\d+)$#', 
-			$data['lastUpdate'], 
-			$match
-		);
+        foreach ($data['phrasesAssociations']['items'] as $phrase) {
+            $phrasesAssociations[] = [
+                'words' => $phrase['phrase'],
+                'impressions' => (int)preg_replace('#[^0-9]#', '', $phrase['number']),
+            ];
+        }
 
-		$sourceTz = date_default_timezone_get();
-		date_default_timezone_set('UTC');
-		$lastUpdate = mktime(
-			0, 0, 0, 
-			$match['month'], 
-			$match['day'], 
-			$match['year']
-		);
-		date_default_timezone_set($sourceTz);
+        preg_match(
+            '#(?<day>\d+)\.(?<month>\d+)\.(?<year>\d+)$#',
+            $data['lastUpdate'],
+            $match
+        );
 
-		return new Result(
-			$impressions, 
-			$includingPhrases,
-			$phrasesAssociations,
-			$lastUpdate,
-			$nextPageExists
-		);
-	}
+        $sourceTz = date_default_timezone_get();
+        date_default_timezone_set('UTC');
+        $lastUpdate = mktime(
+            0, 0, 0,
+            $match['month'],
+            $match['day'],
+            $match['year']
+        );
+        date_default_timezone_set($sourceTz);
 
-	protected function decodeData(array $data)
-	{
-		$nodejs = new NodeRunner();
-		$nodejs->setSource(file_get_contents(__DIR__ . '/Guzzle6/decode.js'));
-		try {
-			$decoded = $nodejs->call('decode', $data, $this->getUserAgent());
-		} catch (\Exception $ex) {
-			throw new BrowserException('The result decode failed', null, $ex);
-		}
+        return new Result(
+            $impressions,
+            $includingPhrases,
+            $phrasesAssociations,
+            $lastUpdate,
+            $nextPageExists
+        );
+    }
 
-		$decoded = @json_decode(urldecode($decoded), true); 
-		if (!$decoded) {
-			throw new BrowserException('The result is an invalid json');
-		}
-		if (!isset($decoded['content'])) {
-			throw new BrowserException('The result is wrong or the format has been changed');
-		}
-		
-		return $decoded['content'];
-	}
+    protected function decodeData(array $data)
+    {
+        $nodejs = new NodeRunner();
+        $nodejs->setSource(file_get_contents(__DIR__ . '/Guzzle6/decode.js'));
+        try {
+            $decoded = $nodejs->call('decode', $data, $this->getUserAgent());
+        } catch (\Exception $ex) {
+            throw new BrowserException('The result decode failed', null, $ex);
+        }
+
+        $decoded = @json_decode(urldecode($decoded), true);
+        if (!$decoded) {
+            throw new BrowserException('The result is an invalid json');
+        }
+        if (!isset($decoded['content'])) {
+            throw new BrowserException('The result is wrong or the format has been changed');
+        }
+
+        return $decoded['content'];
+    }
 }
